742c1e4f73d54a3213b8317a1f81aa5feecb995d
Vladimir Petrenko <94476626+Valdenko007@users.noreply.github.com>
2022-07-24 20:29:53 +0300

    Update 1.sql

--- 1.sql
+++ 1.sql
@@ -1,8 +1,8 @@
 alter table developers
 add salary int not null default 0;
 
-UPDATE `goit_hw_db`.`developers` SET `salary` = '1000' WHERE (`id_developer` = '1');
-UPDATE `goit_hw_db`.`developers` SET `salary` = '2000' WHERE (`id_developer` = '2');
-UPDATE `goit_hw_db`.`developers` SET `salary` = '3000' WHERE (`id_developer` = '3');
-UPDATE `goit_hw_db`.`developers` SET `salary` = '2000' WHERE (`id_developer` = '4');
-UPDATE `goit_hw_db`.`developers` SET `salary` = '4000' WHERE (`id_developer` = '5');
+UPDATE `developers` SET `salary` = '1000' WHERE (`id_developer` = '1');
+UPDATE `developers` SET `salary` = '2000' WHERE (`id_developer` = '2');
+UPDATE `developers` SET `salary` = '3000' WHERE (`id_developer` = '3');
+UPDATE `developers` SET `salary` = '2000' WHERE (`id_developer` = '4');
+UPDATE `developers` SET `salary` = '4000' WHERE (`id_developer` = '5');
